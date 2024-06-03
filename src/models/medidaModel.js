10b93b7b1f305ded5b18dd11ef6feb71814fc3a4
FerAmorim <160801292+euFernandoAmorim@users.noreply.github.com>
2024-06-02 22:42:13 -0300

    dashboard quase lá

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -4,22 +4,19 @@ function buscarUltimasMedidas() {
 
     var instrucaoSql = `select grupo_muscular.nome as musculo, count(curtir) as curtidas, count(descurtir) as descurtidas from grupo_muscular
     join curtida on fkgrupo = idgrupo
-    where fkGrupo = 1
+
     group by curtida.fkgrupo `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(fkGrupo) {
+function buscarMedidasEmTempoReal() {
 
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${idAquario} 
-                    ORDER BY id DESC LIMIT 1`;
+    var instrucaoSql = `select grupo_muscular.nome as musculo, fkgrupo, count(curtir) as curtidas, count(descurtir) as descurtidas from grupo_muscular
+    join curtida on fkgrupo = idgrupo
+
+    group by curtida.fkgrupo`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
